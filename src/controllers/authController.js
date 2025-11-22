806c81bc2957b5899708b8ee25e017d35f8e5049
Mahesh Veeraboina <maheshveeraboina632@gmail.com>
2025-11-22 18:39:21 +0530

    Replace bcrypt with bcryptjs and prepare for Render deploy

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 // src/controllers/authController.js
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { Organisation, User, Log } = require('../models');
 
